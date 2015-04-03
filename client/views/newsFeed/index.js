05e4429521db3a478a22d838b320a860f1af49b5
maxim <uzumaxy@gmail.com>
2015-04-03 09:18:58 +0300

    Code has been reformatted.

--- client/views/newsFeed/index.js
+++ client/views/newsFeed/index.js
@@ -1,40 +1,38 @@
-Template.newsFeedIndex.helpers({
-
-});
+Template.newsFeedIndex.helpers({});
 
 Template.newsFeedIndex.events = {
-    'click #btnRemove': function(e) {
+    'click #btnRemove': function (e) {
         e.preventDefault();
         if (confirm("Are you sure want to remove this data?"))
             Router.current().remove(this._id);
     },
     /* sorting by parameter */
-'click #btnSortuserID': function(e) {
-MeteorisGridView.sort('userId');
-},
-/* sorting by parameter */
-'click #btnSortdateCreated': function(e) {
-MeteorisGridView.sort('dateCreated');
-},
-/* sorting by parameter */
-'click #btnSortdateStatusChanged': function(e) {
-MeteorisGridView.sort('dateStatusChanged');
-},
-/* sorting by parameter */
-'click #btnSortstatus': function(e) {
-MeteorisGridView.sort('status');
-},
-/* sorting by parameter */
-'click #btnSortpostID': function(e) {
-MeteorisGridView.sort('postID');
-},
+    'click #btnSortuserID': function (e) {
+        MeteorisGridView.sort('userId');
+    },
+    /* sorting by parameter */
+    'click #btnSortdateCreated': function (e) {
+        MeteorisGridView.sort('dateCreated');
+    },
+    /* sorting by parameter */
+    'click #btnSortdateStatusChanged': function (e) {
+        MeteorisGridView.sort('dateStatusChanged');
+    },
+    /* sorting by parameter */
+    'click #btnSortstatus': function (e) {
+        MeteorisGridView.sort('status');
+    },
+    /* sorting by parameter */
+    'click #btnSortpostID': function (e) {
+        MeteorisGridView.sort('postID');
+    },
 
-    'keyup #search': function(e, t) {
+    'keyup #search': function (e, t) {
         e.preventDefault();
-        Router.current().search(t);        
+        Router.current().search(t);
     },
     /* check all checkbox */
-    'change #checkAll': function(e) {
+    'change #checkAll': function (e) {
         e.preventDefault();
         var checkboxes = $('.checkAll');
         for (var i = 0, n = checkboxes.length; i < n; i++) {
@@ -42,11 +40,12 @@ MeteorisGridView.sort('postID');
         }
     },
     /* remove all selected item */
-    'click #btnRemoveAll': function(e) {
+    'click #btnRemoveAll': function (e) {
         e.preventDefault();
         var checkboxes = $('.checkAll');
         var checkedLength = 0;
-        for (var i = 0; i < checkboxes.length; i++) {
+        var i;
+        for (i = 0; i < checkboxes.length; i++) {
             if (checkboxes[i].checked) {
                 checkedLength++;
             }
@@ -55,7 +54,7 @@ MeteorisGridView.sort('postID');
         if (checkedLength > 0) {
             if (confirm("Are you sure want to remove? (total " + checkedLength + " data will be removed)")) {
                 // loop over them all
-                for (var i = 0; i < checkboxes.length; i++) {
+                for (i = 0; i < checkboxes.length; i++) {
                     // And stick the checked ones onto an array...
                     if (checkboxes[i].checked) {
                         Router.current().remove($(checkboxes[i]).val());
