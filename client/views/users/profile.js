7aadc76fef3c625259eb68e717be88d878ef421f
Taras Dolynyuk <trsdln@gmail.com>
2015-04-20 13:45:50 +0300

    Date picker for dob in user profile

--- client/views/users/profile.js
+++ client/views/users/profile.js
@@ -1,29 +1,35 @@
-Template.usersProfile.created = function(){
-    Meteor.subscribe('interests', Meteor.userId());
-}
+Template.usersProfile.created = function () {
+  Meteor.subscribe('interests', Meteor.userId());
+};
+
+Template.usersProfile.rendered = function () {
+  this.$('.date-of-birth').datepicker({
+    format: "dd/mm/yyyy"
+  });
+};
 
 Template.usersProfile.events = {
-    'click #btnChangeProfile': function(e, t){
-        e.preventDefault();
-        Router.current().changeProfile(t);
-    },
-    'click #btnChangePassword': function(e, t){
-        e.preventDefault();        
-        Router.current().changePassword(t);
-    },
-    'click .change-username-btn': function(e, t){
-        var element = $('#name')[0];
-        Meteor.call('UserProfile.changeUsername', element.value, function(err, res){
-            console.log(err || res);
-        });
-    }
+  'click #btnChangeProfile': function (e, t) {
+    e.preventDefault();
+    Router.current().changeProfile(t);
+  },
+  'click #btnChangePassword': function (e, t) {
+    e.preventDefault();
+    Router.current().changePassword(t);
+  },
+  'click .change-username-btn': function (e, t) {
+    var element = $('#name')[0];
+    Meteor.call('UserProfile.changeUsername', element.value, function (err, res) {
+      console.log(err || res);
+    });
+  }
 };
 
 Template.usersProfile.helpers({
-    getSelected: function (acctType, currentValue) {
-        if (currentValue == acctType) return 'selected'
-    },
-    allowChangeName: function(){
-        return !Meteor.user().usernameEdited
-    }
+  getSelected: function (acctType, currentValue) {
+    if (currentValue == acctType) return 'selected'
+  },
+  allowChangeName: function () {
+    return !Meteor.user().usernameEdited
+  }
 });
