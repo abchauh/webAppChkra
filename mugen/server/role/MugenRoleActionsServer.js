30d4a2d545417361745b8f181905f39389df281b
ahmadthd <ahmed@techchaps.co.uk>
2015-04-29 13:05:12 +0100

    seed data added to mugen-roles to enable access to all collections through demo admin account.

--- mugen/server/role/MugenRoleActionsServer.js
+++ mugen/server/role/MugenRoleActionsServer.js
@@ -80,16 +80,196 @@ Meteor.startup(function() {
         MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "1", mugenRoleGroupId:"1"});        
         MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "1", mugenRoleGroupId:"1"});        
         MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "1", mugenRoleGroupId:"1"});        
-        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "1", mugenRoleGroupId:"1"});                
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "1", mugenRoleGroupId:"1"});
         //posts
         MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "2"});
-        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "2"});        
-        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "2", mugenRoleGroupId:"1"});        
-        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "2", mugenRoleGroupId:"1"});        
-        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "2", mugenRoleGroupId:"1"});        
-        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "2", mugenRoleGroupId:"2"});        
-        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "2", mugenRoleGroupId:"2"});        
-        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "2", mugenRoleGroupId:"2"});        
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "2"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "2", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "2", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "2", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "2", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "2", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "2", mugenRoleGroupId:"2"});
+          //campaigns
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "4"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "4"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "4", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "4", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "4", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "4", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "4", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "4", mugenRoleGroupId:"2"});
+        //campUpdates
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "5"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "5"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "5", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "5", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "5", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "5", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "5", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "5", mugenRoleGroupId:"2"});
+        //causes
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "6"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "6"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "6", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "6", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "6", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "6", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "6", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "6", mugenRoleGroupId:"2"});
+        //causesUpdates
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "7"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "7"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "7", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "7", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "7", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "7", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "7", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "7", mugenRoleGroupId:"2"});
+        //feeds
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "8"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "8"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "8", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "8", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "8", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "8", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "8", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "8", mugenRoleGroupId:"2"});
+        //followLists
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "9"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "9"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "9", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "9", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "9", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "9", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "9", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "9", mugenRoleGroupId:"2"});
+        //images
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "10"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "10"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "10", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "10", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "10", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "10", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "10", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "10", mugenRoleGroupId:"2"});
+        //likeProj
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "11"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "11"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "11", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "11", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "11", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "11", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "11", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "11", mugenRoleGroupId:"2"});
+        //messages
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "12"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "12"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "12", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "12", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "12", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "12", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "12", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "12", mugenRoleGroupId:"2"});
+        //meteorSkills
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "13"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "13"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "13", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "13", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "13", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "13", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "13", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "13", mugenRoleGroupId:"2"});
+        //notifications
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "14"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "14"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "14", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "14", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "14", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "14", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "14", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "14", mugenRoleGroupId:"2"});
+        //owners
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "15"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "15"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "15", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "15", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "15", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "15", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "15", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "15", mugenRoleGroupId:"2"});
+        //likePosts
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "16"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "16"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "16", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "16", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "16", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "16", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "16", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "16", mugenRoleGroupId:"2"});
+        //projects
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "17"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "17"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "17", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "17", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "17", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "17", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "17", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "17", mugenRoleGroupId:"2"});
+        //projUpdates
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "18"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "18"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "18", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "18", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "18", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "18", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "18", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "18", mugenRoleGroupId:"2"});
+        //promoted
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "19"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "19"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "19", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "19", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "19", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "19", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "19", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "19", mugenRoleGroupId:"2"});
+        //suggests
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "20"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "20"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "20", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "20", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "20", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "20", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "20", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "20", mugenRoleGroupId:"2"});
+        //votes
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "21"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "21"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "21", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "21", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "21", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "21", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "21", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "21", mugenRoleGroupId:"2"});
+        //blog
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "22"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "22"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "22", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "22", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "22", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "22", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "22", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "22", mugenRoleGroupId:"2"});
+        //newsFeed
+        MugenRoleActions.insert({name: "index", mugenRoleCollectionId: "3"});
+        MugenRoleActions.insert({name: "view", mugenRoleCollectionId: "3"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "3", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "3", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "3", mugenRoleGroupId:"1"});
+        MugenRoleActions.insert({name: "insert", mugenRoleCollectionId: "3", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "update", mugenRoleCollectionId: "3", mugenRoleGroupId:"2"});
+        MugenRoleActions.insert({name: "remove", mugenRoleCollectionId: "3", mugenRoleGroupId:"2"});
     }
 });
 
--- mugen/server/role/MugenRoleCollectionsServer.js
+++ mugen/server/role/MugenRoleCollectionsServer.js
@@ -62,6 +62,26 @@ Meteor.startup(function() {
     if (mugenRoleCollections.count() == 0) {
         MugenRoleCollections.insert({_id: "1", name: "users"});
         MugenRoleCollections.insert({_id: "2", name: "posts"});
+        MugenRoleCollections.insert({_id: "3", name: "newsFeed"});
+        MugenRoleCollections.insert({_id: "4", name: "campaigns"});
+        MugenRoleCollections.insert({_id: "5", name: "campUpdates"});
+        MugenRoleCollections.insert({_id: "6", name: "causes"});
+        MugenRoleCollections.insert({_id: "7", name: "causesUpdates"});
+        MugenRoleCollections.insert({_id: "8", name: "feeds"});
+        MugenRoleCollections.insert({_id: "9", name: "followLists"});
+        MugenRoleCollections.insert({_id: "10", name: "images"});
+        MugenRoleCollections.insert({_id: "11", name: "likeProj"});
+        MugenRoleCollections.insert({_id: "12", name: "messages"});
+        MugenRoleCollections.insert({_id: "13", name: "meteorSkills"});
+        MugenRoleCollections.insert({_id: "14", name: "notifications"});
+        MugenRoleCollections.insert({_id: "15", name: "owners"});
+        MugenRoleCollections.insert({_id: "16", name: "likePosts"});
+        MugenRoleCollections.insert({_id: "17", name: "projects"});
+        MugenRoleCollections.insert({_id: "18", name: "projUpdates"});
+        MugenRoleCollections.insert({_id: "19", name: "promoted"});
+        MugenRoleCollections.insert({_id: "20", name: "suggests"});
+        MugenRoleCollections.insert({_id: "21", name: "votes"});
+        MugenRoleCollections.insert({_id: "22", name: "blog"});
     }
 });
 
