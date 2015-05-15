c7224b9a22ed59c4192156b4422cc5bd1f4b32fa
Dinesh Nagar <dineshnagar69@users.noreply.github.com>
2015-05-15 20:04:59 +0530

    Update header.js
    Sidebar click event.

--- client/templates/basic/header.js
+++ client/templates/basic/header.js
@@ -4,5 +4,18 @@ Template.templateBasicHeader.events({
         Meteor.logout(function(){
             Router.go('usersLogin');
         });          
+    },
+
+    'click #sidebar': function() {
+        console.log('sidebar click event');
+        $('.content').show();
+        $('.left.iconBar.sidebar')
+            .sidebar('setting', 'transition', 'overlay')
+            .sidebar('toggle');
+    },
+
+    'mouseover #iconBar': function() {
+        console.log('checkpoint1');
+        $('.content').show();
     }
 });
