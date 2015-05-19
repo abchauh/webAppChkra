6d15345a2a224e593766e90bedb0479478592d7d
Dinesh Nagar <dineshnagar69@users.noreply.github.com>
2015-05-19 19:30:09 +0530

    Create home.js
    respected js of home.html

---
+++ client/templates/basic/home.js
@@ -0,0 +1,43 @@
+/**
+ * Created by root on 18/5/15.
+ */
+Template.templateHome.helpers({
+    users: [
+        {   user: {
+                campaignOwner : "Campaign Owner",
+                displayName : "user",
+                userName : "@username",
+                followers : "34.k",
+                campaign : 1 ,
+                img : '/images/macaw/IMG.jpg',
+            }
+        },
+        {   user: {
+                campaignOwner : "Campaign Owner",
+                displayName : "user",
+                userName : "@username",
+                followers : "34.k",
+                campaign : 1 ,
+                img : '/images/macaw/IMG.jpg',
+            }
+        },
+        {   user: {
+                campaignOwner : "Campaign Owner",
+                displayName : "user",
+                userName : "@username",
+                followers : "34.k",
+                campaign : 1 ,
+                img : '/images/macaw/IMG.jpg',
+            }
+        },
+        {   user: {
+                campaignOwner : "Campaign Owner",
+                displayName : "user",
+                userName : "@username",
+                followers : "34.k",
+                campaign : 1 ,
+                img : '/images/macaw/IMG.jpg',
+            }
+        },
+    ]
+});
